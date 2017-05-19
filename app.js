57e27048b099287dd8ccee0b0c46b0a45762a1fa
Mark Staebler <markstaebler@gmail.com>
2017-05-18 17:29:27 -0700

    vue setup

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
 app.listen(3000, function () {
